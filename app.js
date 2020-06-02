0c0ab9110d920e4fc1edd04c7e8290968a5e0d63
Ashutosh Yadav <accesssdenied777@gmail.com>
2020-06-03 01:40:43 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-const routes = require('./routes');
-
-console.log(routes.text);
-
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
