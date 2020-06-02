4402a890467257f71a3bc80da2714077c27676a0
Ashutosh Yadav <accesssdenied777@gmail.com>
2020-06-03 01:42:01 +0530

    Update roues.js

--- Assignment 1/roues.js
+++ Assignment 1/roues.js
@@ -16,16 +16,16 @@ const requestHandler = (req,res) => {
         res.write('</html>');
         return res.end();
     }
-    // if(url==='/users')
-    // {
-    //     res.write('<html>');
-    //     res.write('<head><title> Test routes </title></head>');
-    //     res.write('<body>');
-    //     res.write('<ul><li>User 0</li> <li>User 1</li> <li>User 2</li> <li>User 3</li> <li>User 4</li></ul>')
-    //     res.write('</body>');
-    //     res.write('</html>');
-    //     return res.end();
-    // }
+    if(url==='/users')
+    {
+        res.write('<html>');
+        res.write('<head><title> Test routes </title></head>');
+        res.write('<body>');
+        res.write('<ul><li>User 0</li> <li>User 1</li> <li>User 2</li> <li>User 3</li> <li>User 4</li></ul>')
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
     if(url==='/create-user' && method==='POST')
     {
         const body=[]; //data chunk ke form mai aa raha wse aarrray mai daal raha
